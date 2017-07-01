382dc76821e0e59ee3d95a0a294389ad6227f973
Will <willczubiak@gmail.com>
2017-06-30 20:19:48 -0700

    changed code to make it easier for multiple subreddits

--- app.py
+++ app.py
@@ -3,57 +3,59 @@ import pdb
 import re
 import os
 
+# This allows me to use the same code for multiple subreddits
+def subCheck(file, sub, numPosts):
+    # Opens a file full of acronyms for talesfromtechsupport
+    with open(file, "r") as f:
+        temp = f.read()
+        temp = temp.split("\n")
+        temp = list(filter(None, temp))
+
+    # Creates a list the acronyms from the file.
+    abr = []
+    # Splits each acronym and meaning into a 2D array
+    for l in temp:
+        abr.append(l.split("_"))
+
+
+    subreddit = reddit.subreddit(sub)
+    for submission in subreddit.hot(limit=numPosts):
+        #print(submission.title)
+
+        # If we haven't replied to this post before
+        if submission.id not in posts_replied_to:
+
+            post = False
+            reply = "Acronym|Meaning\n:--|:--\n"
+            for item in abr:
+                if re.search(item[0], submission.selftext, re.IGNORECASE):
+                    # Store the current id into our list
+                    post = True
+
+                    reply += item[0] + "|" + item[1] + "\n"
+            reply += "\n*This was an automated response.*  \n*Please PM me if there were any mistakes.*"
+
+            # If an abreviation was found, then post a reply, and store post in replied to file
+            if (post):
+                posts_replied_to.append(submission.id)
+                submission.reply(reply)
+                print("Bot replying to : ", submission.title)
 
 # Create the Reddit instance
 reddit = praw.Reddit('bot')
 
-
-# Have we run this code before? If not, create an empty list
+# Creates file incase it doesn't exist
 if not os.path.isfile("posts_replied_to.txt"):
     posts_replied_to = []
 
-# If we have run the code before, load the list of posts we have replied to
 else:
-    # Read the file into a list and remove any empty values
+    # Creates an array of posts replied to
     with open("posts_replied_to.txt", "r") as f:
         posts_replied_to = f.read()
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = list(filter(None, posts_replied_to))
-        
-with open("tfts.txt", "r") as f:
-    temp = f.read()
-    temp = temp.split("\n")
-    temp = list(filter(None, temp))
-print temp
-tfts = []
-for l in temp:
-    tfts.append(l.split("_"))
-print tfts
-# Get the top 5 values from our subreddit
-subreddit = reddit.subreddit('pythonforengineers')
-for submission in subreddit.hot(limit=2):
-    #print(submission.title)
 
-    # If we haven't replied to this post before
-    if submission.id not in posts_replied_to:
-        
-        post = False
-        reply = "Acronym|Meaning\n:--|:--\n"
-        for abr in tfts:
-            if re.search(abr[0], submission.selftext, re.IGNORECASE):
-                # Store the current id into our list
-                post = True
-                
-                reply += abr[0] + "|" + abr[1] + "\n"
-        reply += "\n*This was an automated response.*  \n*Please PM me if there were any mistakes.*"
-                
-        # If an abreviation was found, then post a reply, and store post in replied to file
-        if (post):
-            posts_replied_to.append(submission.id)
-            submission.reply(reply)
-            print("Bot replying to : ", submission.title)
-
-        
+subCheck("tfts.txt", "pythonforengineers", 5)
 
 # Write our updated list back to the file
 with open("posts_replied_to.txt", "w") as f:
